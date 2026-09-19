087625343c2dce5a272e3de094a09b149acb30ae
Rohith-Balamukund <f20250762@pilani.bits-pilani.ac.in>
2026-09-19 16:18:45 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,24 +4,37 @@
 module tb;
 
   // TODO: declare the inputs and outputs
+  reg [2:0] sel_test_input ;
+  wire [7:0] test_output ;
+  integer x;
+  parameter TEST_DEPTH = 8;
 
-  // TODO: instantiate DUT here
+  // Initiating lut instance here
+  lut #(.DEPTH(TEST_DEPTH), .WIDTH(8)) my_lut (
+    .sel(sel_test_input), 
+    .dout(test_output)
+  ) ;
+
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
-      $dumpvars(0, DUT);
+      $dumpvars(0, my_lut);
     end
   end
 
   initial begin
     // TODO: apply different input combinations
+    sel_test_input = 0;
+    for(x=0; x<TEST_DEPTH; x++) begin
+      #5 sel_test_input = x;
+    end
 
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " Input =%d | Output=%d", sel_test_input, test_output); // change as required
 
 endmodule
